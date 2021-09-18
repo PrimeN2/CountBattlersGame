1754be32e6be76ea2278cb12b5c2e663d2ea33fa
Prime_N2 <nikitaprime83@gmail.com>
2021-09-18 23:41:15 +0400

    -fix bug with incorrect data save

--- Assets/Scripts/BinarySaveSystem.cs
+++ Assets/Scripts/BinarySaveSystem.cs
@@ -5,29 +5,14 @@ using UnityEngine;
 public class BinarySaveSystem : ISaveSystem
 {
     private readonly string _filePath;
-    private const string _isFirstLaunchKey = "IsFirstLaunch";
 
     public BinarySaveSystem()
     {
         _filePath = Application.persistentDataPath + "/Save.dat";
-        if (!PlayerPrefs.HasKey(_isFirstLaunchKey))
-        {
-            PlayerPrefs.SetInt(_isFirstLaunchKey, 1);
-            PlayerPrefs.Save();
-        }
     }
 
     public void Save(PlayerData playerData)
     {
-        if (PlayerPrefs.HasKey(_isFirstLaunchKey))
-        {
-            if(PlayerPrefs.GetInt(_isFirstLaunchKey) == 1)
-            {
-                PlayerPrefs.SetInt(_isFirstLaunchKey, 0);
-                PlayerPrefs.Save();
-            }
-
-        }
         using (FileStream file = File.Create(_filePath))
         {
             new BinaryFormatter().Serialize(file, playerData);
@@ -36,17 +21,18 @@ public class BinarySaveSystem : ISaveSystem
 
     public PlayerData Load()
     {
-        if (PlayerPrefs.HasKey(_isFirstLaunchKey))
-        {
-            if (PlayerPrefs.GetInt(_isFirstLaunchKey) == 1)
-                return new PlayerData(0, 0);
-        }
-
         PlayerData saveData;
-        using (FileStream file = File.Open(_filePath, FileMode.Open))
+        try
         {
-            object loadedData = new BinaryFormatter().Deserialize(file);
-            saveData = (PlayerData)loadedData;
+            using (FileStream file = File.Open(_filePath, FileMode.Open))
+            {
+                object loadedData = new BinaryFormatter().Deserialize(file);
+                saveData = (PlayerData)loadedData;
+            }
+        }
+        catch
+        {
+            saveData = new PlayerData(0, 0);
         }
 
         return saveData;
